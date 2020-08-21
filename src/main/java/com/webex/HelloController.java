e0dcb90f4c3daa48351a0fafe4f9f9309ac220b4
big <igbyun86@gmail.com>
2020-08-21 23:49:40 +0900

    Add RequestMapping page

--- src/main/java/com/webex/HelloController.java
+++ src/main/java/com/webex/HelloController.java
@@ -1,9 +1,19 @@
 package com.webex;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloController {
 
+    @RequestMapping(method = RequestMethod.GET, value="/home")
+    public String main(ModelMap model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+        return "/home";
+    }
 }
